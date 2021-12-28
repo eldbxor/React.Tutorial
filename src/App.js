63c9ea82cc5444dd56953cf4a9f26887da4a6f76
yootaek <eldbxor@naver.com>
2021-12-28 16:11:16 +0900

    First init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+import IterationSample from "./IterationSample";
+import ScrollBox from "./ScrollBox";
+
+class App extends Component {
+  render() {
+    return <IterationSample />;
+  }
+}
+
+export default App;
